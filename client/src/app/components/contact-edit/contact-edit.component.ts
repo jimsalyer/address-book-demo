476afb8b8e6265b005c15330026498ba99fd1a76
Jim Salyer <jim.salyer@gmail.com>
2021-01-25 11:12:51 -0600

    added delete functionality

--- client/src/app/components/contact-edit/contact-edit.component.ts
+++ client/src/app/components/contact-edit/contact-edit.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { Contact } from './../../models/contact';
 import { ContactService } from './../../services/contact.service';
 
@@ -10,15 +11,18 @@ import { ContactService } from './../../services/contact.service';
   styleUrls: ['./contact-edit.component.scss']
 })
 export class ContactEditComponent implements OnInit {
+  @ViewChild('modalContent') modalContent?: any;
+
   contact = new Contact();
   form: FormGroup;
   id = 0;
 
   constructor(
+    private formBuilder: FormBuilder,
+    private modalService: NgbModal,
     private route: ActivatedRoute,
     private router: Router,
-    private formBuilder: FormBuilder,
-    private service: ContactService
+    private contactService: ContactService
   ) {
     this.form = this.formBuilder.group({
       contactId: [0, [Validators.required, Validators.pattern(/^\d+$/)]],
@@ -39,7 +43,7 @@ export class ContactEditComponent implements OnInit {
   ngOnInit(): void {
     this.id = +(this.route.snapshot.params.id ?? 0);
     if (this.id > 0) {
-      this.service.getContact(this.id).subscribe(
+      this.contactService.getContact(this.id).subscribe(
         (contact: Contact) => {
           this.contact = contact;
           this.form.setValue(contact);
@@ -49,17 +53,28 @@ export class ContactEditComponent implements OnInit {
     }
   }
 
+  onDelete(): void {
+    this.modalService.open(this.modalContent).result.then(
+      () => {
+        this.contactService.deleteContact(this.id).subscribe(
+          () => this.router.navigate(['/contacts'])
+        );
+      },
+      () => {}
+    );
+  }
+
   onReset(): void {
     this.form.reset(this.contact);
   }
 
   onSubmit(): void {
     if (this.id > 0) {
-      this.service.updateContact(this.id, this.form.value).subscribe(
+      this.contactService.updateContact(this.id, this.form.value).subscribe(
         () => this.router.navigate(['/contacts'])
       );
     } else {
-      this.service.addContact(this.form.value).subscribe(
+      this.contactService.addContact(this.form.value).subscribe(
         () => this.router.navigate(['/contacts'])
       );
     }
