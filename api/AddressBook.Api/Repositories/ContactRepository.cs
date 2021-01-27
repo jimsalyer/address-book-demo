5d36bb03587a83225b781e284cbe4f4f1252cc5e
Jim Salyer <jim.salyer@gmail.com>
2021-01-27 10:47:14 -0600

    consolidated interfaces into their respective class files

--- api/AddressBook.Api/Repositories/ContactRepository.cs
+++ api/AddressBook.Api/Repositories/ContactRepository.cs
@@ -9,6 +9,15 @@ using Sieve.Services;
 
 namespace AddressBook.Api.Repositories
 {
+    public interface IContactRepository
+    {
+        Task<Contact> AddContactAsync(ContactDto contactDto);
+        Task<Contact> DeleteContactAsync(int id);
+        Task<Contact> GetContactAsync(int id);
+        Task<List<Contact>> ListContactsAsync(string filters, string sorts, string defaultSorts);
+        Task<Contact> UpdateContactAsync(int id, ContactDto contactDto);
+    }
+
     public class ContactRepository : IContactRepository
     {
         private readonly AddressBookDbContext _dbContext;
--- api/AddressBook.Api/Services/ContactService.cs
+++ api/AddressBook.Api/Services/ContactService.cs
@@ -5,6 +5,15 @@ using AddressBook.Api.Repositories;
 
 namespace AddressBook.Api.Services
 {
+    public interface IContactService
+    {
+        Task<Contact> AddContactAsync(ContactDto contactDto);
+        Task<Contact> DeleteContactAsync(int id);
+        Task<Contact> GetContactAsync(int id);
+        Task<List<Contact>> ListContactsAsync(string filters, string sorts);
+        Task<Contact> UpdateContactAsync(int id, ContactDto contactDto);
+    }
+
     public class ContactService : IContactService
     {
         private readonly IContactRepository _contactRepository;
