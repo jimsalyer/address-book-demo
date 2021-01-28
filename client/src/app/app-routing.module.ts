bf3931574b1b8fb30775bc12a405c80a73d05725
Jim Salyer <jim.salyer@gmail.com>
2021-01-28 13:21:25 -0600

    fixed some minor routing issues

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,16 +1,15 @@
-import { LoginComponent } from './components/login/login.component';
-import { AuthGuard } from './guards/auth.guard';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ContactEditComponent } from './components/contact-edit/contact-edit.component';
 import { ContactListComponent } from './components/contact-list/contact-list.component';
 import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
+import { AuthGuard } from './guards/auth.guard';
 
 const routes: Routes = [
-  { path: '', component: HomeComponent },
+  { path: '', component: HomeComponent, canActivate: [AuthGuard] },
   { path: 'contacts', component: ContactListComponent, canActivate: [AuthGuard] },
-  { path: 'contacts/new', component: ContactEditComponent },
-  { path: 'contacts/:id', component: ContactEditComponent },
+  { path: 'contacts/:id', component: ContactEditComponent, canActivate: [AuthGuard] },
   { path: 'login', component: LoginComponent },
   { path: '**', redirectTo: '' }
 ];
